41e11286bee8dbe48e8746e7a456400f93dc739a
Naomi Seyfer <naomi@seyfer.org>
2023-10-15 22:53:29 +0100

    Better system for post-init hooks

--- lib/mod.lua
+++ lib/mod.lua
@@ -1,4 +1,34 @@
 local mod = require 'core/mods'
+local hook = require 'core/hook'
+local tab = require 'tabutil'
+-- Begin post-init hack block
+if hook.script_post_init == nil and mod.hook.patched == nil then
+    mod.hook.patched = true
+    local old_register = mod.hook.register
+    local post_init_hooks = {}
+    mod.hook.register = function(h, name, f)
+        if h == "script_post_init" then
+            post_init_hooks[name] = f
+        else
+            old_register(h, name, f)
+        end
+    end
+    mod.hook.register('script_pre_init', '!replace init for fake post init', function()
+        local old_init = init
+        init = function()
+            old_init()
+            for i, k in ipairs(tab.sort(post_init_hooks)) do
+                local cb = post_init_hooks[k]
+                print('calling: ', k)
+                local ok, error = pcall(cb)
+                if not ok then
+                    print('hook: ' .. k .. ' failed, error: ' .. error)
+                end
+            end
+        end
+    end)
+end
+-- end post-init hack block
 
 local menu = require('matrix/lib/menu')
 local matrix = require('matrix/lib/matrix')
@@ -7,16 +37,17 @@ mod.hook.register("system_post_startup", "hack the matrix", function()
     matrix:install()
 end)
 
-mod.hook.register("script_pre_init", "install matrix post-init hooks", function()
-    local old_init = init
+mod.hook.register("script_post_init", "aa metrix mod depth params", function()
+    matrix:add_modulation_depth_params()
+end)
+
+mod.hook.register("script_post_init", "~ matrix one last param read", function()
+    matrix:call_post_init_hooks() -- preserved for back compat
+    params:read(nil, true)
+end)
+
+mod.hook.register("script_pre_init", "maybe this is not necessary", function()
     menu.page = nil
-    init = function()
-        old_init()
-        matrix:add_modulation_depth_params()
-        matrix:call_post_init_hooks()
-        -- One last params read. Don't bang except the things we explicitly deferred a bang of.
-        params:read(nil, true)
-    end
 end)
 
 mod.hook.register("script_post_cleanup", "clear the matrix for the next script", function()
